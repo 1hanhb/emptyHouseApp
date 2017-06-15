1bbb415055475c014c93d0007633f481dacd5f43
munak <oiool@naver.com>
2017-06-16 02:15:27 +0900

    qq

--- app/models/home.rb
+++ app/models/home.rb
@@ -10,11 +10,11 @@ has_attached_file :avatar, :styles => {
 
 validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
-validates :title, presence: { message: "제목을 입력해 주십시오." }
+validates :title, length: { minimum: 4, maximum: 20 ,message: "제목은 최소 4자 - 최대 20자 입니다." }
 validates :houseAddress, presence: { message: "주소를 입력해 주십시오." }
 validates :detailedAddress, presence: { message: "상세주소를 입력해 주십시오." }
-validates :housingDeposit, presence: { message: "보증금을 입력해 주십시오." }
-validates :monthlyFee, presence: { message: "월세를 입력해 주십시오." }
+validates :housingDeposit, numericality: { greater_than_or_equal_to: 0, message: "보증금을 0 이상 입력해 주십시오." }
+validates :monthlyFee, presence: { greater_than_or_equal_to: 0, message: "월세를 0 이상 입력해 주십시오." }
 validates :avatar, presence: { message: "사진을 등록해 주십시오." }
 
 
